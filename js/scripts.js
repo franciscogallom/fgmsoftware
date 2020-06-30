c36df62457d9a54e104e23b074a4f75c04eb7411
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-06-30 20:53:44 -0300

    Mejoras en el responsive y el idioma

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,60 @@ let color3 = '#cf0';
 
 // VANILLA JAVASCRIPT
 
-let btnShow = document.getElementById('btn-show');
-let textParrafo = document.getElementById('parrafo');
 
-function changeText () {
-    textParrafo.innerHTML = '"Si tu negocio no está en internet, no existe". Bill Gates.';
-    btnShow.remove();
+// let btnShow = document.getElementById('btn-show');
+// let textParrafo = document.getElementById('parrafo');
+
+// function changeText () {
+//     textParrafo.innerHTML = '"Si tu negocio no está en internet, no existe". Bill Gates.';
+//     btnShow.remove();
+// }
+
+// btnShow.onclick = changeText;
+
+
+// TYPING
+
+let i=0;
+let text = 'website development to boost your business.'
+
+function typing() {
+    if (i<text.length) {
+        document.getElementById('text-h').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,75)
+    }
 }
 
-btnShow.onclick = changeText;
+typing();
+
+// TYPING
+
+// VANILLA JAVASCRIPT
 
 // SHADOW NUMBER
 
-let num1 = document.getElementById('number-m1');
-let num2 = document.getElementById('number-m2');
-let num3 = document.getElementById('number-m3');
-let num4 = document.getElementById('number-m4');
-let num5 = document.getElementById('number-m5');
+// let num1 = document.getElementById('number-m1');
+// let num2 = document.getElementById('number-m2');
+// let num3 = document.getElementById('number-m3');
+// let num4 = document.getElementById('number-m4');
+// let num5 = document.getElementById('number-m5');
 
-let shadow = '';
+// let shadow = '';
 
 // for (let i=0; i < 750; i++) {
 //     shadow +=(shadow? ',' : '')+i*1+'px '+ i*1+'px 1px #698300'
 // }
 
-num1.style.textShadow = shadow;
-num2.style.textShadow = shadow;
-num3.style.textShadow = shadow;
-num4.style.textShadow = shadow;
-num5.style.textShadow = shadow;
+// num1.style.textShadow = shadow;
+// num2.style.textShadow = shadow;
+// num3.style.textShadow = shadow;
+// num4.style.textShadow = shadow;
+// num5.style.textShadow = shadow;
 
 // SHADOW NUMBER
+
+
 
 // Ejemplo de addEventListener
 // btnShow.addEventListener('mouseover', function () {
